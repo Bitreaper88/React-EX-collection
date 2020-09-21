8aa21bafbf230c4d4da9a0ae3a00a38de3f78493
JussiLipponen <bitreaper88@gmail.com>
2020-09-21 09:45:54 +0300

    complete ex1

---
+++ code_4/ex1/ex1.ts
@@ -0,0 +1,26 @@
+complelet count: number = 3;
+
+function countDown() {
+    switch (count) {
+        case 3: {
+            console.log("3");
+            break;
+        }
+        case 2: {
+            console.log("2");
+            break;
+        }
+        case 1: {
+            console.log("1");
+            //break; 
+        }
+        default: {
+            console.log("GO!");
+            break;
+        }
+    }
+    count--;
+    if (0 < count) setTimeout(countDown, 1000);    
+}
+
+countDown();
