16ca4b68d63d992e28550d174170b1b35a47550c
Bit_reaper <jussi@js-games.com>
2020-10-17 09:23:24 +0300

    home work works

--- home_6/memory/src/App.tsx
+++ home_6/memory/src/App.tsx
@@ -1,97 +1,132 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useImperativeHandle } from 'react';
 import './App.css';
 
 interface ICard{
   id: number;
   card: string;
-  matched: boolean
+  matched: boolean;
+  visible: boolean;
 }
 
+const cardTypes: Array<string> =["apple", "orange", "banana", "pear", "coconut", "strawberry", "mango", "blueberry"];
+const cardTypesX2: Array<string> = cardTypes.concat(cardTypes); // this just produces a new array with two of each type
+let cardDeck: ICard[]
+
 function App() {
+  
+  let tempDeck = cardTypesX2.map((value, index) => {
+    let newCard: ICard = { id: index, card: value, matched: false, visible: false };
+    return newCard
+  })
+  tempDeck.sort(() => Math.random() - 0.5);
+  cardDeck = tempDeck;
   return (
-  <div className="grid-container">  
-    <AllCards />
-  </div>
-  );
+      <AllCards/>
+  )
+
 }
 export default App;
 
-
-let selectedCards: ICard[] = [];
-let matchedCardsArray:  ICard[] = [];
+let paircount: number = 0;
+let selectedCard1: ICard | null = null
+let selectedCard2: ICard | null = null
 
 const Card: React.FC<ICard> = props => {
-  const {id, card, matched } = props;
- // let [selectedCards, setSelectedCards] = useState(() => selectedCardsArray);
-  let [matchedCard, setMatchedCard] = useState(() => matchedCardsArray);
-  const [show, setShow] = useState(false);
+  let {id, card, matched, visible} = props;
+// let [selectedCards, setSelectedCards] = useState(() => selectedCardsArray);
+//let [matchedCard, setMatchedCard] = useState(() => matchedCardsArray);
 
-  
-  const flip = () => {
-    if(!matched){
-      if (selectedCards.length < 2){
-        setShow(!show);
-        selectedCards.push({id, card, matched });
-       // setSelectedCards(selectedCardsArray);
-        console.log(selectedCards);
-      }    
-
-    }else alert("Already matched");
-
-    if (selectedCards.length >= 2) {
-      console.log("selectedCards ")
-      console.log(selectedCards[0].card )
-      console.log(selectedCards[1].card )
-
-      if (selectedCards[0].card === selectedCards[1].card) {
-        //setMatched(true);
-        console.log("Two cards")
-        selectedCards.forEach((card) => {
-          matchedCardsArray.push(card)
-          console.log("Push: " + card.card )
-        })
-        console.log("matched array" + matchedCardsArray); 
-        selectedCards = new Array();
-       
+  function flip() {
+    if (!matched) {
+      if (selectedCard1) {
+        console.log("The first card " + selectedCard1.card);
+        console.log("Selected the second card card " + card);
+        console.log(selectedCard1.card);
+        console.log(card)
+        visible = true;
+        cardDeck[id].visible = visible;
+        selectedCard2 = { id, card, matched, visible }
+      } 
+      
+      if (selectedCard1 === null){
+        selectedCard2 = null;
+        visible = true;
+        cardDeck[id].visible = visible;
+        selectedCard1 = { id, card, matched, visible }
+        console.log("Selected the first card " + selectedCard1.card);
       }
-     alert("trigger timer")
+
+    } else visible = true;
+
+    if (!selectedCard1 || !selectedCard2) return;
+    if (selectedCard1.card === selectedCard2.card) {
+      let id1 = selectedCard1.id;
+      let id2 = selectedCard2.id;
+      cardDeck[id1].matched = true;
+      cardDeck[id2].matched = true;
+      selectedCard1 = null;
+      selectedCard2 = null;
+      paircount++;
     }
   }
-  
+
   return (
-    <div className={"grid-item"} onClick={() => flip()}>
+    <div className={"grid-item"} onClick={flip}>
       {(() => {
-        if (show) {
+        if (visible) {
          return (<div className="showing">{card}</div>);
-        }
+        }else return (<div className="noShowing"> </div>);
       })()}
     </div>
   );
 }
 
-// function Timer = () => {
-//   useEffect(() => {
-//     let intervalId;
-
-//     if (true) {
-//       intervalId = setInterval(() => {
-
-//       }, 1000)
-//     }
-// }
-
 
 const AllCards: React.FC = () => {
-  const cardTypes: Array<string> =["apple", "orange", "banana", "pear", "coconut", "strawberry", "mango", "blueberry"];
 
-  let cardDeck: Array<string> = cardTypes.concat(cardTypes); // this just produces a new array with two of each type
-  cardDeck.sort(() => Math.random() - 0.5);
+  let [displayCards, setDisplayCards] =  useState(
+    cardDeck.map((value, index) => {
+    return  <Card key={index} id={index} card={value.card} matched={value.matched} visible={value.visible}/>
+  }));
+
+  function ReDraw () {
+    console.log("redraw");
+    setDisplayCards(
+      cardDeck.map((value, index) => {
+      return  <Card key={index} id={index} card={value.card} matched={value.matched} visible={value.visible}/>
+    }));
+  }
+
+  useEffect(() =>{
+    console.log("effect " + selectedCard1?.card);
+    console.log("effect " + selectedCard2?.card);
+    if(selectedCard1 && selectedCard2){
+      let id1 = selectedCard1.id;
+      let id2 = selectedCard2.id;
+      setTimeout(()=>{
+        selectedCard1 = null;
+        selectedCard2 = null;
+        cardDeck[id1].visible = false;
+        cardDeck[id2].visible = false;
+        ReDraw()
+       
+      
+       }, 2000)
+    }
+    console.log("paircount " + paircount);
+    if (paircount === 8){
+      alert("You Win!!!");
+    }
+  });
+
 
   return (
     <>
-      {cardDeck.map((value, index) => {
-        return  <Card key={index} id={index} card={value} matched={false}/>
-      })}
+      <div className="grid-container"onClick={ReDraw} >
+        {displayCards}
+      </div>
     </>
   )
 }
+
+
